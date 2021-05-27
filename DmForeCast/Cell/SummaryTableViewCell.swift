ca99a78a48ff54e59d9418251a6da8634b126893
dongmin9117 <ehdals5108@gmail.com>
2021-05-27 20:48:01 +0900

    수정

---
+++ DmForeCast/Cell/SummaryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SummaryTableViewCell.swift
+//  DmForeCast
+//
+//  Created by Walter yun on 2021/05/27.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
